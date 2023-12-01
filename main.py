475b8d2e885e7c13698ec4dedd2c22e85f0415c4
whsprite <62704231+whsprite@users.noreply.github.com>
2023-12-01 09:49:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import torch
 import torchvision.datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-from models import MNIST_target_net
-from models import Net
 from WGAN import WGAN_Attack
 import os
 
@@ -14,21 +12,14 @@ if __name__ == "__main__":
     batch_size = 128
     BOX_MIN = 0
     BOX_MAX = 1
+    model_num_labels = 10
 
-    # Define what device we are using
     print("CUDA Available: ",torch.cuda.is_available())
     device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
 
-    #Load classfier
-    # pretrained_model = "./MNIST_target_model_origin.pth"
-    pretrained_model = "./MLP_target_model.pth"
-    # targeted_model = MNIST_target_net().to(device)
-    targeted_model = Net().to(device)
-    targeted_model.load_state_dict(torch.load(pretrained_model))
-    targeted_model.eval()
-    model_num_labels = 10
-
-    # MNIST train dataset and dataloader declaration
+    #load model
+    ##### targeted_model = .....
+    
     mnist_dataset = torchvision.datasets.MNIST('./dataset', train=True, transform=transforms.ToTensor(), download=True)
     dataloader = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
     WGAN = WGAN_Attack(device,
